f4c4eb82ee18a9047c14265bebfbefe0362842ed
larry9315 <larry9315@gmail.com>
2025-07-23 11:51:33 -0700

    created and started about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+
+export default function AboutPage() {
+  return (
+    <section className="min-h-screen bg-white px-8 py-16 text-center">
+      <h1 className="text-4xl font-bold text-black mb-4">About Me</h1>
+      <p className="text-lg text-gray-700">
+        This is the about page.
+      </p>
+    </section>
+  );
+}
